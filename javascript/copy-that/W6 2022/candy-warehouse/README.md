3ae4cffbca94007eaffc0b708f127675e7ced1e1
Michael Taggart <mtaggarthawk@gmail.com>
2022-02-02 21:17:52 -0700

    candy-warehouse started

---
+++ javascript/copy-that/W6 2022/candy-warehouse/README.md
@@ -0,0 +1,3 @@
+# Copy-That
+
+Try to copy the following [website](https://www.candywarehouse.com/).
---
+++ javascript/copy-that/W6 2022/candy-warehouse/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// use antd package
+// using antd create a search bar
+// using the antd package create a contact us button with image
+// using the antd package create a cart button with image
+// using the antd package create a logo
+// using the antd package create a navbar with the logo and the search bar
+// using the antd package create a Wish List button with image
+// using the antd package create a cart icon with image
+// using the antd package create below a banner with a list of the following: Bulk Candy, Types of Candy, Candy Flavors, Candy Shapes, Themes, Occasions, Holidays, International, Brands, Sale
+
+function NavBar() {
+  return <></>;
+}
+
+export default NavBar;
