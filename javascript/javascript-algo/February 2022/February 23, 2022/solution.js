2c296e9699888d3f5ad232a4a4c796628336a1ea
Michael Taggart <mtaggarthawk@gmail.com>
2022-02-25 08:26:38 -0700

    February 23, 2022

--- javascript/javascript-algo/February 2022/February 23, 2022/solution.js
+++ javascript/javascript-algo/February 2022/February 23, 2022/solution.js
@@ -1,72 +1,13 @@
-// You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
-
-// Given the following input array:
-
-// var list1 = [
-//   { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
-//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null },
-//   { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
-// ];
-// write a function that
-
-// adds the question property to each object in the input array where the developer has not provided the relevant details (marked with a null value in JavaScript, with the default value in COBOL). The value of the question property should be the following string:
-// Hi, could you please provide your <property name>.
-
-// and returns only the developers with missing details:
-// [
-//   { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java', 
-//   question: 'Hi, could you please provide your firstName.' },
-//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null, 
-//   question: 'Hi, could you please provide your language.' }
-// ]
-// Notes:
-
-// At most only one of the values will be null / empty.
-// Preserve the order of the original list.
-// Return an empty array [] if there is no developer with missing details.
-// The input array will always be valid and formatted as in the example above.
-
-//Test Cases:
-// describe("Tests", () => {
-//  it("test", () => {
-// var list1 = [
-//  { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
-//  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null },
-//  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
-// ];
-
-// var answer1 = [
-//  { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java', 
-//  question: 'Hi, could you please provide your firstName.' },
-//  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null, 
-//  question: 'Hi, could you please provide your language.' }
-// ];
-
-// Test.assertDeepEquals(askForMissingDetails(list1), answer1);
-
-
-// var list2 = [
-//  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
-//  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
-//  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
-// ];
-
-// var answer2 = [];
-
-// Test.assertDeepEquals(askForMissingDetails(list2), answer2);
-
-//  });
-// });
-
-//Solution 1:
 function askForMissingDetails(list) {
-  return list.map((developer) => {
-    let missingDetails = [];
-    for (let key in developer) {
-      if (developer[key] === null) {
-        missingDetails.push(key);
+  let missingDetails = [];
+  for (let i = 0; i < list.length; i++) {
+      let obj = list[i];
+      for (let key in obj) {
+          if (obj[key] === null) {
+              obj.question = 'Hi, could you please provide your ' + key + ".";
+              missingDetails.push(obj);
+          }
       }
-    }
-    return { ...developer, question: `Hi, could you please provide your ${missingDetails.join(', ')}` };
-  });
+  }
+  return missingDetails;
 }
