9f6e4f02d44e9f8094b4d5978bd5a1f7403e07c7
Michael Taggart <mtaggarthawk@gmail.com>
2022-03-01 10:38:14 -0700

    March 1, 2022

---
+++ javascript/javascript-algo/March 2022/March 1, 2022/solution.js
@@ -0,0 +1,17 @@
+function consecutiveOnes(nums) {
+  let max = 0;
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      count++;
+      if (count > max) {
+        max = count;
+      }
+    } else {
+      count = 0;
+    }
+  }
+
+  return max;
+};
